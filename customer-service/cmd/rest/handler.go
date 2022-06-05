1c1799ee1fc8164e9ebbde39de5269271e31fa97
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-06-05 20:04:25 +0700

    add get all customers and top up balance

--- customer-service/cmd/rest/handler.go
+++ customer-service/cmd/rest/handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"customer-service/internal/use_case"
 	"encoding/json"
 	"github.com/google/uuid"
 	"lib"
@@ -9,6 +10,8 @@ import (
 
 type CustomerService interface {
 	RegisterCustomer(name string) (*uuid.UUID, error)
+	GetAllCustomers() ([]use_case.CustomerDetail, error)
+	TopUp(customerId uuid.UUID, amount float64) error
 }
 
 type Handler struct {
@@ -30,6 +33,38 @@ func (h Handler) registerCustomer(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	productId, err := h.customerService.RegisterCustomer(request.Name)
+	if err != nil {
+		lib.WriteResponse(w, err, http.StatusBadRequest, nil)
+		return
+	}
 
 	lib.WriteResponse(w, err, http.StatusCreated, productId)
+}
+
+func (h Handler) getCustomer(w http.ResponseWriter, _ *http.Request) {
+	customers, err := h.customerService.GetAllCustomers()
+	if err != nil {
+		lib.WriteResponse(w, err, http.StatusInternalServerError, nil)
+		return
+	}
+	lib.WriteResponse(w, err, http.StatusOK, customers)
+}
+
+func (h Handler) topUpBalance(w http.ResponseWriter, r *http.Request) {
+	type requestBody struct {
+		CustomerId uuid.UUID `json:"customer_id"`
+		Amount     float64   `json:"amount"`
+	}
+	var request requestBody
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		lib.WriteResponse(w, lib.NewErrBadRequest(err.Error()), http.StatusBadRequest, nil)
+		return
+	}
+	err = h.customerService.TopUp(request.CustomerId, request.Amount)
+	if err != nil {
+		lib.WriteResponse(w, err, http.StatusBadRequest, nil)
+		return
+	}
+	lib.WriteResponse(w, err, http.StatusOK, nil)
 }
