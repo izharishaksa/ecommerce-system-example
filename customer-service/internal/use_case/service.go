f74fd4351c06d3fada2dfe3b44267aeddc37c71a
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-06-06 18:47:47 +0700

    renamed and fixed dependency

--- customer-service/internal/use_case/service.go
+++ customer-service/internal/use_case/service.go
@@ -5,15 +5,21 @@ import (
 	"github.com/google/uuid"
 )
 
-type CustomerService struct {
-	customerRepository CustomerRepository
+type CustomerService interface {
+	RegisterCustomer(name, email string) (*uuid.UUID, error)
+	GetAllCustomers() ([]CustomerDetail, error)
+	TopUp(customerId uuid.UUID, amount float64) error
 }
 
-func NewCustomerService(customerRepository CustomerRepository) *CustomerService {
-	return &CustomerService{customerRepository: customerRepository}
+type customerService struct {
+	customerRepository customer.Repository
 }
 
-func (service *CustomerService) RegisterCustomer(name, email string) (*uuid.UUID, error) {
+func NewCustomerService(customerRepository customer.Repository) CustomerService {
+	return &customerService{customerRepository: customerRepository}
+}
+
+func (service *customerService) RegisterCustomer(name, email string) (*uuid.UUID, error) {
 	customerInstance, err := customer.NewCustomer(name, email)
 	if err != nil {
 		return nil, err
@@ -25,7 +31,7 @@ func (service *CustomerService) RegisterCustomer(name, email string) (*uuid.UUID
 	return &customerInstance.Id, err
 }
 
-func (service *CustomerService) GetAllCustomers() ([]CustomerDetail, error) {
+func (service *customerService) GetAllCustomers() ([]CustomerDetail, error) {
 	customers, err := service.customerRepository.GetCustomer()
 	if err != nil {
 		return nil, err
@@ -41,7 +47,7 @@ func (service *CustomerService) GetAllCustomers() ([]CustomerDetail, error) {
 	return customerDetails, nil
 }
 
-func (service *CustomerService) TopUp(customerId uuid.UUID, amount float64) error {
+func (service *customerService) TopUp(customerId uuid.UUID, amount float64) error {
 	customerInstance, err := service.customerRepository.FindCustomerById(customerId)
 	if err != nil {
 		return err
