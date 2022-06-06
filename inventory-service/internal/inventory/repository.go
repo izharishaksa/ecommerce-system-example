0950c8ba8495a41ceabc02037f668c3fc58052f3
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-06-06 18:28:53 +0700

    moved to domain

---
+++ inventory-service/internal/inventory/repository.go
@@ -0,0 +1,11 @@
+package inventory
+
+import (
+	"github.com/google/uuid"
+)
+
+type Repository interface {
+	SaveProduct(product *Product) error
+	FindProductById(id uuid.UUID) (*Product, error)
+	GetAllProducts() ([]*Product, error)
+}
