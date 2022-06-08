871f71067e6560b0c3208b8a7e8c87096cb34d21
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-06-08 12:02:11 +0700

    Update repository_in_memory.go

--- order-service/internal/order/repository_in_memory.go
+++ order-service/internal/order/repository_in_memory.go
@@ -1,5 +1,10 @@
 package order
 
+import (
+	"github.com/google/uuid"
+	"lib"
+)
+
 type inMemoryRepository struct {
 	orders map[string]*Order
 }
@@ -10,7 +15,23 @@ func NewInMemoryRepository() Repository {
 	}
 }
 
+func (repo inMemoryRepository) FindOrderById(id uuid.UUID) (*Order, error) {
+	order, ok := repo.orders[id.String()]
+	if !ok {
+		return nil, lib.NewErrNotFound("order not found")
+	}
+	return order, nil
+}
+
 func (repo inMemoryRepository) SaveOrder(order *Order) error {
 	repo.orders[order.Id.String()] = order
 	return nil
+}
+
+func (repo inMemoryRepository) GetAllOrders() ([]Order, error) {
+	var orders []Order
+	for _, order := range repo.orders {
+		orders = append(orders, *order)
+	}
+	return orders, nil
 }
