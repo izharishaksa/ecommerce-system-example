2f9a494d11f27f062d63121c0e9366eaa4701435
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-06-08 12:01:23 +0700

    deleted unrequired entity

--- order-service/internal/inventory/repository.go
+++
@@ -1,7 +0,0 @@
-package inventory
-
-import "github.com/google/uuid"
-
-type Repository interface {
-	GetProductAvailability(productIds []uuid.UUID) ([]ProductDetail, error)
-}
--- order-service/internal/inventory/response.go
+++
@@ -1,9 +0,0 @@
-package inventory
-
-import "github.com/google/uuid"
-
-type ProductDetail struct {
-	ProductId    uuid.UUID
-	CurrentPrice float64
-	Stock        int
-}
