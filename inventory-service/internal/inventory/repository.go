b8fd2baecabcf4850dbc1ba7f7523545ef850483
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-06-08 11:57:33 +0700

    add stocks checker and updater

--- inventory-service/internal/inventory/repository.go
+++ inventory-service/internal/inventory/repository.go
@@ -8,4 +8,6 @@ type Repository interface {
 	SaveProduct(product *Product) error
 	FindProductById(id uuid.UUID) (*Product, error)
 	GetAllProducts() ([]Product, error)
+	GetAvailableStock(ids []uuid.UUID) (*Stock, error)
+	UpdateStock(stock *Stock) error
 }
