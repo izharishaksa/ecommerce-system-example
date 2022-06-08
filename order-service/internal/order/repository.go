dc77422ea57f5d1ab74c9d46945ff9d5f82304fb
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-06-08 12:02:09 +0700

    Update repository.go

--- order-service/internal/order/repository.go
+++ order-service/internal/order/repository.go
@@ -1,5 +1,9 @@
 package order
 
+import "github.com/google/uuid"
+
 type Repository interface {
 	SaveOrder(order *Order) error
+	GetAllOrders() ([]Order, error)
+	FindOrderById(id uuid.UUID) (*Order, error)
 }
