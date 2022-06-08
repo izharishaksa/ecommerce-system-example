b4e61e3564cfb748d3367e5ace8037f10ab8729a
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-06-08 11:56:25 +0700

    create kafka consumer and rest api runner

--- inventory-service/cmd/main.go
+++ inventory-service/cmd/main.go
@@ -2,71 +2,71 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"golang.org/x/sync/errgroup"
-	"inventory-service/cmd/kafka"
+	"github.com/segmentio/kafka-go"
+	"inventory-service/cmd/kafkaclient"
 	"inventory-service/cmd/rest"
+	"inventory-service/internal/inventory"
+	"inventory-service/internal/use_case"
 	"lib"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
-func iterate(ctx context.Context) {
-	g, _ := errgroup.WithContext(ctx)
-	for ii := 5; ii < 6; ii++ {
-		i := ii
-		g.Go(func() error {
-			fmt.Println("Looping...", i)
-			for {
-				select {
-				case <-ctx.Done():
-					fmt.Println("Break the loop ", i)
-					return nil
-				case <-time.After(time.Duration(i) * time.Second):
-					fmt.Println("Hello in a loop ", i)
-				}
-			}
-		})
-	}
-	err := g.Wait()
-	if err != nil {
-		fmt.Println("Error group: ", err)
-	}
-	fmt.Println("inside iteration done")
+func exampleHandler(message kafka.Message) error {
+	log.Printf("message: %s %s", message.Topic, message.Value)
+
+	return nil
 }
 
 func main() {
 	cfg := lib.LoadConfigByFile("./cmd", "config", "yaml")
-	var err error
 	var ctx, cancel = context.WithCancel(context.Background())
+
+	//setup service
+	kafkaWriter := &kafka.Writer{
+		Addr:                   kafka.TCP(cfg.Kafka),
+		AllowAutoTopicCreation: true,
+	}
+	inventoryRepository := inventory.NewInMemoryRepository()
+	inventoryService := use_case.NewInventoryService(inventoryRepository, kafkaWriter)
+
+	//setup rest handler
+	restChan := make(chan error, 1)
 	go func() {
-		interruption := make(chan os.Signal, 1)
-		signal.Notify(interruption, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGINT)
-		<-interruption
-		cancel()
+		restHandler := rest.NewHandler(inventoryService)
+		restChan <- rest.Run(cfg, restHandler)
 	}()
 
-	g, _ := errgroup.WithContext(ctx)
-	g.Go(func() error {
-		err = rest.Run(cfg)
-		if err != nil {
-			log.Println(err)
-		}
-		return err
-	})
+	//setup kafka consumer handler
+	topics := []string{"ORDER_PLACED", "ORDER_CREATED", "ORDER_CANCELED"}
+	consumerErrChan := make(chan error, len(topics))
+	kafkaConsumerHandler := kafkaclient.NewHandler(inventoryService)
+	go func() {
+		consumerErrChan <- kafkaclient.Consume(ctx, cfg, "ORDER_PLACED", "ORDER_PLACED_GROUP", kafkaConsumerHandler.PlacedOrder)
+	}()
+	go func() {
+		consumerErrChan <- kafkaclient.Consume(ctx, cfg, "ORDER_CREATED", "ORDER_CREATED_GROUP", exampleHandler)
+	}()
+	go func() {
+		consumerErrChan <- kafkaclient.Consume(ctx, cfg, "ORDER_CANCELED", "ORDER_CANCELED_GROUP", exampleHandler)
+	}()
 
-	g.Go(func() error {
-		topics := []string{"ORDER_PLACED", "ORDER_CREATED", "ORDER_CANCELED", "ORDER_REJECTED", "ORDER_PAID"}
-		kafka.Consume(ctx, cfg, topics)
-		return nil
-	})
+	interruption := make(chan os.Signal)
+	go func() {
+		signal.Notify(interruption, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGINT)
+	}()
 
-	err = g.Wait()
-	if err != nil {
-		fmt.Println("Error group: ", err)
+	<-interruption
+	cancel()
+
+	select {
+	case <-interruption:
+		log.Println("Interrupted")
+	case err := <-consumerErrChan:
+		log.Println("consumer ran with an error", err)
+	case err := <-restChan:
+		log.Println("rest ran with an error", err)
 	}
-	fmt.Println("Main done")
 }
